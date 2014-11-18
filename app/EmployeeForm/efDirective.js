4f7177d7dfa5beab8f57e09e3dde5f21f4e81b0f
lexucuflexu <alex.tenche@gmail.com>
2014-11-18 20:08:00 +0200

    finished c3 - all ok

--- app/EmployeeForm/efDirective.js
+++ app/EmployeeForm/efDirective.js
@@ -1,7 +1,10 @@
-angularFormsApp.directive('employeeForm', 
-	function(){
-		return{
-			restrict: 'E',
-			templateUrl: 'app/EmployeeForm/efTemplate.html'
-		}
-	});
+﻿
+angularFormsApp.directive('employeeForm',
+    function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'app/EmployeeForm/efTemplate.html'
+        }
+
+    });
