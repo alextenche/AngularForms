ffb8929050abdc8ec29880e1a4ea155bb4ee4b08
lexucuflexu <alex.tenche@gmail.com>
2014-11-18 09:38:18 +0200

    progress

--- app/EmployeeForm/efController.js
+++ app/EmployeeForm/efController.js
@@ -1,4 +1,12 @@
 angularFormsApp.controller('efController',
 	function efController($scope, efService){
-		$scope.employee = efService.employee;	
+	
+		$scope.employee = efService.employee;
+		
+		$scope.departments = [
+			"Engineering",
+            "Marketing",
+            "Finance",
+            "Administration"
+		];
 });
